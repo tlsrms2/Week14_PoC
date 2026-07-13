45b0455b7d25085a40e884684c53d12326f2fbf0
KSY511710 <sugaga1238@gmail.com>
2026-07-13 21:16:02 +0900

    Hog보스 스톤 애니메이션 추가

--- Assets/Scripts/Boss/Hog/HogBossAI.cs
+++ Assets/Scripts/Boss/Hog/HogBossAI.cs
@@ -1,14 +1,54 @@
+using UnityEngine;
 using Week14.Audio;
 
 namespace Week14.Enemy
 {
     public sealed partial class HogBossAI : GraphBossAI
     {
+        private static readonly int StunParameter = Animator.StringToHash("Stun");
+        private static readonly int EndStunParameter = Animator.StringToHash("EndStun");
+
+        [SerializeField] private Animator groggyAnimator;
+
         protected override bool RotatesBodyToPlayer => false;
 
         protected override void OnCombatStarted()
         {
             SoundManager.PlayBgm("HogBgm");
+        }
+
+        protected override void OnHpEmptyBegan()
+        {
+            SetAnimatorTrigger(StunParameter);
+        }
+
+        protected override void OnHpEmptyRecovered()
+        {
+            SetAnimatorTrigger(EndStunParameter);
+        }
+
+        private void SetAnimatorTrigger(int parameter)
+        {
+            Animator targetAnimator = ResolveGroggyAnimator();
+            if (targetAnimator == null)
+            {
+                return;
+            }
+
+            targetAnimator.SetTrigger(parameter);
+        }
+
+        private Animator ResolveGroggyAnimator()
+        {
+            if (groggyAnimator != null)
+            {
+                return groggyAnimator;
+            }
+
+            groggyAnimator = BodyRoot != null
+                ? BodyRoot.GetComponentInChildren<Animator>(true)
+                : GetComponentInChildren<Animator>(true);
+            return groggyAnimator;
         }
     }
 }
